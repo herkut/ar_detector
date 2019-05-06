4054d1a676ea03274afc2b4a4eb9d1e8d9ba6f97
herkut <erkuthamdi@gmail.com>
2019-05-06 14:13:53 +0300

    Initialized 1st dnn model

--- models/dnn.py
+++ models/dnn.py
@@ -1,33 +1,161 @@
+import collections
+import json
+import os
+
 from keras import Sequential
-from keras.layers import Dense, BatchNormalization
+from keras.engine.saving import model_from_json
+from keras.layers import Dense, BatchNormalization, Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+import numpy as np
+
+from utils.confusion_matrix_drawer import plot_confusion_matrix
+from utils.numpy_encoder import NumpyEncoder
 
 
-class DNN:
-    def __init__(self, feature_size, output_size, hidden_units_arr, activation_functions_arr, batch_normalization_required=False, optimizer='adam'):
-        self.feature_size = feature_size
-        self.output_size = output_size
-        self.hidden_units_arr = hidden_units_arr
-        self.activation_functions_arr = activation_functions_arr
-        self.batch_normalization_required = batch_normalization_required
-        self.optimizer = optimizer
+class ArDetectorByDNN:
+    def __init__(self, results_directory, feature_selection, antibiotic_name, feature_size, hidden_units, activation_functions, label_tags='phenotype', metrics=['accuracy']):
+        self._x_tr = None
+        self._y_tr = None
+        self._x_te = None
+        self._y_te = None
+        self._results_directory = results_directory
+        self._feature_selection = feature_selection
+        self._antibiotic_name = antibiotic_name
+        self._feature_size = feature_size
+        self._label_tags = label_tags
 
-        self.model = self.create_model()
+        self._hidden_units = hidden_units
+        self._activation_functions = activation_functions
+        self._metrics = metrics
 
-    def create_model(self, hidden_units_arr):
+        self._best_model = None
+
+    def create_model(self, batch_normalization_required=False, optimizer='adam', dropout_rate=0.0):
         model = Sequential()
 
         # Initialize first hidden layer
-        model.add(Dense(hidden_units_arr[0], input_dim=self.feature_size, activation=self.activation_functions_arr[0]))
-        if self.batch_normalization_required:
+        model.add(Dense(self._hidden_units[0], input_dim=self._feature_size, activation=self._activation_functions[0]))
+        if batch_normalization_required:
             model.add(BatchNormalization())
-        for i in range(1, len(hidden_units_arr)):
-            model.add(Dense(hidden_units_arr[i], activation=self.activation_functions_arr[i]))
-            if self.batch_normalization_required:
+        model.add(Dropout(dropout_rate))
+        for i in range(1, len(self._hidden_units)):
+            model.add(Dense(self._hidden_units[i], activation=self._activation_functions[i]))
+            if batch_normalization_required:
                 model.add(BatchNormalization())
-        # Initialize output layer which is multilabel classification in our problem
-        model.add(Dense(self.output_size, activation='softmax'))
+            model.add(Dropout(dropout_rate))
+        # Binary classification would be done for each antibiotic separately
+        model.add(Dense(1, activation='sigmoid'))
 
-        model.compile(loss='categorical_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
+        model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=self._metrics)
 
         return model
 
+    def initialize_train_dataset(self, x_tr, y_tr):
+        self._x_tr = x_tr
+        self._y_tr = y_tr
+
+    def initialize_test_dataset(self, x_te, y_te):
+        self._x_te = x_te
+        self._y_te = y_te
+
+    def initialize_datasets(self, x_tr, y_tr, x_te, y_te):
+        self._x_tr = x_tr
+        self._y_tr = y_tr
+        self._x_te = x_te
+        self._y_te = y_te
+
+    def tune_hyperparameters(self, param_grid):
+        model = KerasClassifier(build_fn=self.create_model, verbose=1)
+
+        grid = GridSearchCV(estimator=model,
+                            param_grid=param_grid)
+
+        grid_result = grid.fit(self._x_tr, self._y_tr)
+
+        print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+        means = grid_result.cv_results_['mean_test_score']
+        stds = grid_result.cv_results_['std_test_score']
+        params = grid_result.cv_results_['params']
+        for mean, stdev, param in zip(means, stds, params):
+            print("%f (%f) with: %r" % (mean, stdev, param))
+
+        target_directory = 'dnn_' + self._metrics[0] + '_' + self._label_tags + '_' + self._feature_selection
+
+        if not os.path.exists(self._results_directory + 'best_models/' + target_directory):
+            os.makedirs(self._results_directory + 'best_models/' + target_directory)
+
+        target_directory = 'dnn_' + self._metrics[0] + '_' + self._label_tags + '_' + self._feature_selection
+
+        if not os.path.exists(self._results_directory + 'grid_search_scores/' + target_directory):
+            os.makedirs(self._results_directory + 'grid_search_scores/' + target_directory)
+
+        with open(self._results_directory + 'grid_search_scores/' + target_directory + '/dnn_' + self._antibiotic_name + '.json', 'w') as f:
+            f.write(json.dumps(grid.cv_results_, cls=NumpyEncoder))
+
+        print(grid_result.best_score_)
+        print(grid_result.best_params_)
+
+        self._best_model = grid.best_estimator_.model
+
+        self.save_model(grid.best_estimator_.model)
+
+    def save_model(self, model):
+        target_directory = 'dnn_' + self._metrics[0] + '_' + self._label_tags + '_' + self._feature_selection
+
+        if not os.path.exists(self._results_directory + 'best_models/' + target_directory):
+            os.makedirs(self._results_directory + 'best_models/' + target_directory)
+
+        # save the models into file
+        model_json = model.to_json()
+        with open(self._results_directory + 'best_models/' + target_directory + '/model.json', "w") as json_file:
+            json_file.write(model_json)
+        # save network weights
+        model.save_weights(self._results_directory + 'best_models/' + target_directory + "/dnn_for_" + self._antibiotic_name + '.h5')
+
+    def load_model(self):
+        target_directory = 'dnn_' + self._metrics[0] + '_' + self._label_tags + '_' + self._feature_selection
+
+        # Model reconstruction from JSON file
+        with open(self._results_directory + 'best_models/' + target_directory + '/model.json', 'r') as f:
+            self._best_model = model_from_json(f.read())
+
+        # Load weights into the new model
+        self._best_model.load_weights(self._results_directory + 'best_models/' + target_directory + "/dnn_for_" + self._antibiotic_name + '.h5')
+
+    def test_model(self):
+        y_pred = self._best_model.predict(self._x_te)
+
+        y_pred[y_pred > 0.5] = 1
+        y_pred[y_pred <= 0.5] = 0
+
+        cm = confusion_matrix(self._y_te, y_pred)
+        if np.shape(cm)[0] == 2 and np.shape(cm)[1] == 2:
+            sensitivity = float(cm[0][0]) / np.sum(cm[0])
+            specificity = float(cm[1][1]) / np.sum(cm[1])
+            print('For ' + self._antibiotic_name)
+            print(collections.Counter(self._y_te))
+            print('Sensitivity: ' + str(sensitivity))
+            print('Specificity: ' + str(specificity))
+        else:
+            print('For ' + self._antibiotic_name)
+            print('There has been an error in calculating sensitivity and specificity')
+
+        # Plot non-normalized confusion matrix
+        # plot_confusion_matrix(self._y_te, y_pred, classes=['susceptible', 'resistant'], title='Confusion matrix, without normalization')
+
+        # Plot normalized confusion matrix
+        plot_confusion_matrix(self._y_te, y_pred, classes=['susceptible', 'resistant'], normalize=True, title='Normalized confusion matrix')
+
+        target_directory = 'dnn_' + self._metrics[0] + '_' + self._label_tags + '_' + self._feature_selection
+
+        if not os.path.exists(self._results_directory + 'confusion_matrices/' + target_directory):
+            os.makedirs(self._results_directory + 'confusion_matrices/' + target_directory)
+
+        plt.savefig(self._results_directory + 'confusion_matrices/' + target_directory + '/normalized_dnn_' + self._antibiotic_name + '.png')
+
+        plot_confusion_matrix(self._y_te, y_pred, classes=['susceptible', 'resistant'], normalize=False, title='Confusion matrix')
+
+        plt.savefig(self._results_directory + 'confusion_matrices/' + target_directory + '/dnn_' + self._antibiotic_name + '.png')
--- models/model_manager.py
+++ models/model_manager.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+from models.dnn import ArDetectorByDNN
 from models.random_forest import ARDetectorByRandomForest
 from models.svm_rbf import ARDetectorBySVMWithRBF
 
@@ -8,7 +9,8 @@ from models.svm_rbf import ARDetectorBySVMWithRBF
 ######################################################################
 target_drugs = ['Isoniazid', 'Rifampicin', 'Ethambutol', 'Pyrazinamide', 'Streptomycin', 'Ofloxacin', 'Amikacin']
 label_tags = 'phenotype'
-TRADITIONAL_ML_SCORING = 'f1'
+TRADITIONAL_ML_SCORING = 'accuracy'
+TEST_SIZE = 0.3
 ######################################################################
 
 
@@ -33,7 +35,7 @@ class ModelManager:
     def train_and_test_models(self, results_directory, feature_selection, raw_feature_matrix, raw_labels):
         for i in range(len(target_drugs)):
             x, y = self.filter_out_nan(raw_feature_matrix, raw_labels[:, i])
-            x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y, test_size=0.3)
+            x_train, x_test, y_train, y_test = train_test_split(x, y, stratify=y, test_size=TEST_SIZE)
             print("For the antibiotic " + target_drugs[i])
             print("Size of training dataset " + str(np.shape(x_train)))
             print("Size of test dataset " + str(np.shape(x_test)))
@@ -93,7 +95,18 @@ class ModelManager:
             #                                   #
             #####################################
             if self.enable_dnn:
-                pass
+                ar_detector = ArDetectorByDNN(results_directory,
+                                              feature_selection,
+                                              target_drugs[i],
+                                              np.shape(x_train)[1],
+                                              [256, 128, 64],
+                                              ['relu', 'relu', 'relu'],
+                                              label_tags=label_tags
+                                              )
+
+                self.train_dnn(ar_detector, x_train, y_train)
+
+                self.test_dnn(ar_detector, x_test, y_test)
 
     def filter_out_nan(self, x, y):
         index_to_remove = np.argwhere(np.isnan(y))
@@ -109,8 +122,6 @@ class ModelManager:
         # c_range = [1]
         gamma_range = [0.001, 0.1, 1, 10, 100]
         # gamma_range = [1]
-        #x_tr, y_tr = self.filter_out_nan(feature_matrix_training, labels_matrix_training[:, index_of_antibiotic])
-        #x_te, y_te = self.filter_out_nan(feature_matrix_test, labels_matrix_test[:, index_of_antibiotic])
 
         print('For ' + ar_detector._antibiotic_name + ' feature and label sizes')
         print('Training ' + str(x_tr.shape) + ' ' + str(y_tr.shape))
@@ -127,9 +138,6 @@ class ModelManager:
         n_estimators = [int(x) for x in np.linspace(start=100, stop=500, num=100)]
         max_features = ['sqrt', 'log2', None]
 
-        #x_tr, y_tr = self.filter_out_nan(feature_matrix_training, labels_matrix_training[:, index_of_antibiotic])
-        #x_te, y_te = self.filter_out_nan(feature_matrix_test, labels_matrix_test[:, index_of_antibiotic])
-
         print('For ' + ar_detector._antibiotic_name + ' feature and label sizes')
         print('Training ' + str(x_tr.shape) + ' ' + str(y_tr.shape))
         #print('Test ' + str(x_te.shape) + ' ' + str(y_te.shape))
@@ -141,12 +149,25 @@ class ModelManager:
         print(ar_detector._best_model)
 
     def train_dnn(self, ar_detector, x_tr, y_tr):
-        pass
+        param_grid = dict(epochs=[50, 100, 150, 200, 500],
+                          batch_size=[1, 25, 50, 75, 100],
+                          optimizer=['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam'],
+                          dropout_rate=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+                          batch_normalization_required=[False, True])
+        """
+        param_grid = dict(epochs=[50, 100],
+                          batch_size=[100],
+                          optimizer=['Adam', 'RMSprop'],
+                          dropout_rate=[0.9],
+                          batch_normalization_required=[False, True])
+        """
+        print('For ' + ar_detector._antibiotic_name + ' feature and label sizes')
+        print('Training ' + str(x_tr.shape) + ' ' + str(y_tr.shape))
+
+        ar_detector.initialize_train_dataset(x_tr, y_tr)
+        ar_detector.tune_hyperparameters(param_grid)
 
     def test_svm_with_rbf(self, ar_detector, x_te, y_te):
-        #x_tr, y_tr = self.filter_out_nan(feature_matrix_training, labels_matrix_training[:, index_of_antibiotic])
-        #x_te, y_te = self.filter_out_nan(feature_matrix_test, labels_matrix_test[:, index_of_antibiotic])
-
         print('Test ' + str(x_te.shape) + ' ' + str(y_te.shape))
 
         ar_detector.initialize_test_dataset(x_te, y_te)
@@ -154,9 +175,6 @@ class ModelManager:
         ar_detector.test_model()
 
     def test_random_forest(self, ar_detector, x_te, y_te):
-        #x_tr, y_tr = self.filter_out_nan(feature_matrix_training, labels_matrix_training[:, index_of_antibiotic])
-        #x_te, y_te = self.filter_out_nan(feature_matrix_test, labels_matrix_test[:, index_of_antibiotic])
-
         print('Test ' + str(x_te.shape) + ' ' + str(y_te.shape))
 
         ar_detector.initialize_test_dataset(x_te, y_te)
@@ -164,4 +182,8 @@ class ModelManager:
         ar_detector.test_model()
 
     def test_dnn(self, ar_detector, x_te, y_te):
-        pass
+        print('Test ' + str(x_te.shape) + ' ' + str(y_te.shape))
+
+        ar_detector.initialize_test_dataset(x_te, y_te)
+        # ar_detector.load_model()
+        ar_detector.test_model()
