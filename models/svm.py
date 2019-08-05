77d673e7d919f5266ed9a035d5f1ff68da87fc29
herkut <erkuthamdi@gmail.com>
2019-08-05 15:36:31 +0300

    First commit for statistical tests that would be used during the research

--- models/svm.py
+++ models/svm.py
@@ -111,3 +111,102 @@ class ARDetectorBySVMWithRBF:
         y_pred = pd.Series(y_pred, name="Predicted")
         df_confusion = pd.crosstab(y_true, y_pred)
         df_confusion.to_csv(self._results_directory + 'confusion_matrices/' + self._target_directory + '/dnn_' + self._antibiotic_name + '.csv')
+
+
+class ARDetectorBySVMWithLinear:
+    def __init__(self, target_base_directory, feature_selection, antibiotic_name, label_tags='phenotype', scoring='roc_auc', class_weights=None):
+        self._x_tr = None
+        self._y_tr = None
+        self._x_te = None
+        self._y_te = None
+        self._target_base_directory = target_base_directory
+        self._feature_selection = feature_selection
+        self._label_tags = label_tags
+        if class_weights is None:
+            self._model = svm.SVC(kernel='rbf')
+        else:
+            self._model = svm.SVC(kernel='rbf', class_weights=class_weights)
+        self._best_model = None
+        self._antibiotic_name = antibiotic_name
+        self._scoring = scoring
+        self._target_directory = 'svm_' + self._scoring + '_' + self._label_tags + '_' + self._feature_selection
+
+    def initialize_train_dataset(self, x_tr, y_tr):
+        self._x_tr = x_tr
+        self._y_tr = y_tr
+
+    def initialize_test_dataset(self, x_te, y_te):
+        self._x_te = x_te
+        self._y_te = y_te
+
+    def initialize_datasets(self, x_tr, y_tr, x_te, y_te):
+        self._x_tr = x_tr
+        self._y_tr = y_tr
+        self._x_te = x_te
+        self._y_te = y_te
+
+    def load_model(self):
+        self._best_model = joblib.load(self._target_base_directory + 'best_models/' + self._target_directory + '/svm_rbf_model_for_' + self._antibiotic_name + '.sav')
+
+    def tune_hyperparameters(self, param_grid):
+        model = self._model
+
+        grid = GridSearchCV(estimator=model, param_grid=param_grid, scoring=self._scoring, cv=5, verbose=True, n_jobs=-1)
+        grid.fit(self._x_tr, self._y_tr)
+
+        print(grid)
+
+        if not os.path.exists(self._target_base_directory + 'grid_search_scores/' + self._target_directory):
+            os.makedirs(self._target_base_directory + 'grid_search_scores/' + self._target_directory)
+
+        with open(self._target_base_directory + 'grid_search_scores/' + self._target_directory + '/svm_rbf_' + self._antibiotic_name + '.json', 'w') as f:
+            f.write(json.dumps(grid.cv_results_, cls=NumpyEncoder))
+
+        # summarize the results of the grid search
+        print('Summary of the model:')
+        print(grid.best_score_)
+        print(grid.best_estimator_.nu)
+        print(grid.best_estimator_.gamma)
+
+        self._best_model = grid.best_estimator_
+
+        if not os.path.exists(self._target_base_directory + 'best_models/' + self._target_directory):
+            os.makedirs(self._target_base_directory + 'best_models/' + self._target_directory)
+
+        # save the model to disk
+        filename = self._target_base_directory + 'best_models/' + self._target_directory + '/svm_rbf_model_for_' + self._antibiotic_name + '.sav'
+        joblib.dump(self._best_model, filename)
+
+    def predict_ar(self, x):
+        self._best_model.predict(x)
+
+    def test_model(self):
+        y_pred = self._best_model.predict(self._x_te)
+
+        cm = confusion_matrix(self._y_te, y_pred)
+        if np.shape(cm)[0] == 2 and np.shape(cm)[1] == 2 :
+            sensitivity = float(cm[0][0]) / np.sum(cm[0])
+            specificity = float(cm[1][1]) / np.sum(cm[1])
+            print('For ' + self._antibiotic_name)
+            print(collections.Counter(self._y_te))
+            print('Sensitivity: ' + str(sensitivity))
+            print('Specificity: ' + str(specificity))
+        else:
+            print('For ' + self._antibiotic_name)
+            print('There has been an error in calculating sensitivity and specificity')
+
+        plot_confusion_matrix(self._y_te, y_pred, classes=['susceptible', 'resistant'], normalize=True, title='Normalized confusion matrix')
+
+        if not os.path.exists(self._target_base_directory + 'confusion_matrices/' + self._target_directory):
+            os.makedirs(self._target_base_directory + 'confusion_matrices/' + self._target_directory)
+
+        plt.savefig(self._target_base_directory + 'confusion_matrices/' + self._target_directory + '/normalized_svm_with_rbf_' + self._antibiotic_name + '.png')
+
+        plot_confusion_matrix(self._y_te, y_pred, classes=['susceptible', 'resistant'], normalize=False, title='Confusion matrix')
+
+        plt.savefig(self._target_base_directory + 'confusion_matrices/' + self._target_directory + '/svm_with_rbf_' + self._antibiotic_name + '.png')
+
+        y_true = pd.Series(self._y_te, name="Actual")
+        y_pred = pd.Series(y_pred, name="Predicted")
+        df_confusion = pd.crosstab(y_true, y_pred)
+        df_confusion.to_csv(self._results_directory + 'confusion_matrices/' + self._target_directory + '/dnn_' + self._antibiotic_name + '.csv')
---
+++ utils/statistical_tests/statistical_tests.py
@@ -0,0 +1,96 @@
+import numpy as np
+import json
+from scipy import stats
+
+
+def compare_models_wrt_kfold_cross_validated_paired_t_test(arr1, arr2):
+    k = len(arr1)
+
+    x = np.zeros(k)
+
+    for i in range(0, k):
+        x[i] = arr1[i] - arr2[i]
+
+    m = np.mean(x)
+    s = np.subtract(x, m)
+    s = np.square(s)
+    s = np.sum(s) / (k - 1)
+    t = np.sqrt(k) * m / s
+    if t > stats.t.ppf(1 - 0.025, k - 1):
+        return 1
+    elif t < -stats.t.ppf(1 - 0.025, k - 1):
+        return -1
+    else:
+        # print('We found accuracies that are not significantly different')
+        return 0
+
+
+def choose_best_hyperparameters(json_containing_cross_validation_results):
+    d = json.load(json_containing_cross_validation_results)
+    best_hyperparameter_id = 0
+    for i in range(1, len(d['results'])):
+        res = compare_models_wrt_kfold_cross_validated_paired_t_test(
+            ['results'][best_hyperparameter_id]['validation_accuracies'],
+            d['results'][i]['validation_accuracies'])
+        if res == -1:
+            best_hyperparameter_id = i
+
+    print('Best hyperparameter index: ' + str(best_hyperparameter_id))
+    print(d['parameters'][best_hyperparameter_id])
+
+    return best_hyperparameter_id
+
+
+def compare_models_wrt_5x2cv_paired_t_test(results_model1, results_model2):
+    s_sq = np.zeros(5)
+    p_mat = np.zeros((5, 2))
+    # calculating variance estimates
+
+    for i in range(0, 5):
+        p_mat[i][0] = results_model1[i]['test_accuracies'][0] - results_model2[i]['test_accuracies'][0]
+        p_mat[i][1] = results_model1[i]['test_accuracies'][1] - results_model2[i]['test_accuracies'][1]
+        s_sq[i] = (np.square(p_mat[i][0] - np.mean(p_mat[i])) + np.square(p_mat[i][1] - np.mean(p_mat[i])))
+
+    t = p_mat[1][1] / (np.sum(s_sq) / 5)
+
+    if t > stats.t.ppf(1 - 0.025, 5) or t < -stats.t.ppf(1 - 0.025, 5):
+        if np.mean(p_mat) < 0:
+            # model 2 is better
+            return -1
+        elif np.mean(p_mat) > 0:
+            # model 1 is better
+            return 1
+        else:
+            print('Boss, we have an issue')
+    else:
+        # print('Models are not significantly different')
+        return 0
+
+
+def compare_models_wrt_5x2cv_paired_f_test(results_model1, results_model2):
+    s_sq = np.zeros(5)
+    p_mat = np.zeros((5, 2))
+    # calculating variance estimates
+
+    for i in range(0, 5):
+        p_mat[i][0] = results_model1[i]['test_accuracies'][0] - results_model2[i]['test_accuracies'][0]
+        p_mat[i][1] = results_model1[i]['test_accuracies'][1] - results_model2[i]['test_accuracies'][1]
+        s_sq[i] = (np.square(p_mat[i][0] - np.mean(p_mat[i])) + np.square(p_mat[i][1] - np.mean(p_mat[i])))
+
+    f = np.sum(np.square(p_mat)) / (2 * np.sum(s_sq))
+    print("f estimation: " + str(f) + ' and f value with 95 confidence interval: ' + str(stats.f.ppf(1-0.05, 10, 5)))
+    if f < stats.f.ppf(1-0.05, 10, 5):
+        print('Models are not significantly different')
+    else:
+        print('Models are significantly different')
+
+
+def choose_best_models(result_json_model1, result_json_model2):
+    model1 = json.load(result_json_model1)
+    model2 = json.load(result_json_model2)
+    compare_models_wrt_5x2cv_paired_f_test(model1['results'], model2['results'])
+
+
+if __name__ == '__main__':
+    # compare_models_wrt_5x2cv_paired_f_test([],[])
+    print(stats.f.ppf(1-0.05, 10, 5))
