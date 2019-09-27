361593986a7fac31b9f149ffe9a0cb8f8e35a8fa
herkut <erkuthamdi@gmail.com>
2019-09-27 12:58:29 +0300

    Extract early stopping in another file

---
+++ models/pytorch_models/early_stopping.py
@@ -0,0 +1,52 @@
+import torch
+
+
+class EarlyStopping(object):
+    def __init__(self, metric='loss', mode='min', min_delta=0, patience=10, checkpoint_file='/tmp/dnn_checkpoint.pt'):
+        """
+
+        :param metric: loss, accuracy, f1, sensitivity, specificity, precision
+        :param mode:
+        :param min_delta:
+        :param patience: how many bad epochs is required to stop early
+        """
+        self.metric = metric
+        self.mode = mode
+        self.min_delta = min_delta
+        self.patience = patience
+        self.bad_epoch_count = 0
+        self.best_index = None
+        self.best_metrics = None
+        self.best_model = None
+
+        self.checkpoint_file = checkpoint_file
+
+    def step(self, epoch, results, model):
+        # From Goodfellow's Deep Learning book
+        if self.best_index is None:
+            self.best_index = epoch
+            self.best_metrics = results
+            self.save_checkpoint(epoch, results, model)
+        else:
+            if self.mode == 'min':
+                if self.best_metrics[self.metric] - results[self.metric] > self.min_delta:
+                    # Update best metrics and save checkpoint
+                    self.save_checkpoint(epoch, results, model)
+                else:
+                    self.bad_epoch_count += 1
+            else:
+                if self.best_metrics[self.metric] - results[self.metric] < self.min_delta:
+                    # Update best metrics and save checkpoint
+                    self.save_checkpoint(epoch, results, model)
+                else:
+                    self.bad_epoch_count += 1
+            if self.bad_epoch_count > self.patience:
+                return True
+            else:
+                return False
+
+    def save_checkpoint(self, epoch, results, model):
+        self.best_index = epoch
+        self.best_metrics = results
+        self.bad_epoch_count = 0
+        torch.save(model.state_dict(), self.checkpoint_file)
