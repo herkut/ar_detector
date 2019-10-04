c45cd4fd979d7ed954a0dd1b4442562aefc42876
herkut <erkuthamdi@gmail.com>
2019-10-04 11:25:07 +0300

    Define method used only in testing best model stored in related directory

--- models/model_manager.py
+++ models/model_manager.py
@@ -204,6 +204,87 @@ class ModelManager:
                 self.test_ar_detector(ar_detector,
                                       x_test,
                                       y_test)
+    def test_best_models(self, feature_selection, raw_feature_matrix, raw_labels):
+        for i in range(len(Config.target_drugs)):
+            x, y = self.filter_out_nan(raw_feature_matrix, raw_labels[Config.target_drugs[i]])
+
+            tr_indexes = np.genfromtxt(os.path.join(Config.dataset_index_directory, Config.target_drugs[i] + '_tr_indices.csv'),
+                                       delimiter=' ',
+                                       dtype=np.int32)
+
+            te_indexes = np.genfromtxt(os.path.join(Config.dataset_index_directory, Config.target_drugs[i] + '_te_indices.csv'),
+                                       delimiter=' ',
+                                       dtype=np.int32)
+
+            # Random state is used to make train and test split the same on each iteration
+            if self.data_representation == 'tfidf':
+                x = DataRepresentationPreparer.update_feature_matrix_with_tf_idf(x)
+            elif self.data_representation == 'tfrf':
+                x = DataRepresentationPreparer.update_feature_matrix_with_tf_rf(x, y)
+            elif self.data_representation == 'bm25tfidf':
+                x = DataRepresentationPreparer.update_feature_matrix_with_bm25_tf_idf(x)
+            elif self.data_representation == 'bm25tfrf':
+                x = DataRepresentationPreparer.update_feature_matrix_with_bm25_tf_rf(x, y)
+            else:
+                # Assumed binary data representation would be used
+                pass
+
+            # Update weights of features if necessary
+            x_train = x.loc[tr_indexes].values
+            y_train = y.loc[tr_indexes].values
+            x_test = x.loc[te_indexes].values
+            y_test = y.loc[te_indexes].values
+
+            x = x.values
+            y = y.values
+
+            class_weights_arr = {}
+
+            unique, counts = np.unique(y, return_counts=True)
+
+            class_weights = {0: counts[1] / (counts[0] + counts[1]), 1: counts[0] / (counts[0] + counts[1])}
+
+            print("For the antibiotic " + Config.target_drugs[i])
+            print("Size of training dataset " + str(np.shape(x_train)))
+            print("Size of test dataset " + str(np.shape(x_test)))
+
+            for model in self.models:
+                if model == 'svm_rbf':
+                    ar_detector = ARDetectorBySVMWithRBF(feature_selection,
+                                                         Config.target_drugs[i],
+                                                         class_weights=class_weights)
+
+                elif model == 'svm_linear':
+                    ar_detector = ARDetectorBySVMWithLinear(feature_selection,
+                                                            Config.target_drugs[i],
+                                                            class_weights=class_weights)
+
+                elif model == 'rf':
+                    ar_detector = ARDetectorByRandomForest(feature_selection,
+                                                           Config.target_drugs[i],
+                                                           class_weights=class_weights)
+
+                elif model == 'lr':
+                    ar_detector = ARDetectorByLogisticRegression(feature_selection,
+                                                                 Config.target_drugs[i],
+                                                                 class_weights=class_weights)
+
+                elif model in self.dnn_models:
+                    # convert class weight into numpy matrix
+                    class_weights_numpy = np.array(list(class_weights.items()), dtype=np.float32)
+                    ar_detector = ARDetectorDNN(feature_selection,
+                                                antibiotic_name=Config.target_drugs[i],
+                                                model_name=model,
+                                                feature_size=x_train.shape[1],
+                                                class_weights=class_weights_numpy)
+
+                else:
+                    raise Exception('Unknown model: ' + model)
+
+                # test pretrained model stored in the best models directory
+                self.test_ar_detector(ar_detector,
+                                      x_test,
+                                      y_test)
 
     def filter_out_nan(self, x, y):
         index_to_remove = y[y.isna() == True].index
--- run.py
+++ run.py
@@ -13,6 +13,7 @@ def main():
     Usage: 
         run.py tune_hyperparameters <configuration_file> <models> [--data_representation=<data_representation>]
         run.py train_best_models <configuration_file> <models> [--data_representation=<data_representation>]
+        run.py test_best_models <configuration_file> <models> [--data_representation=<data_representation>]
         run.py execute_experiments <configuration_file> <models> [--data_representation=<data_representation>]
         run.py select_best_model <configuration_file> <directory_containing_results>
         
@@ -51,6 +52,28 @@ def main():
             raw_label_matrix = FeatureLabelPreparer.get_labels_from_file(os.path.join(Config.dataset_directory, 'labels.csv'))
             raw_feature_matrix = FeatureLabelPreparer.get_feature_matrix_from_files(v)
             model_manager.tune_train_and_test_models(k, raw_feature_matrix, raw_label_matrix)
+
+    elif args['test_best_models']:
+        models = args['<models>']
+        results_directory = args['<directory_containing_results>']
+
+        # As Arzucan Ozgur suggested, we focus on the feature selection approach in the reference paper,
+        # please check old_raw_feature_selection file for alternatives
+        raw_feature_selections = {'snp_09_bcf_nu_indel_00_platypus_all': [
+                                        os.path.join(Config.dataset_directory, 'new_approach_with_normalization', 'snp_bcftools_0.9_notunique.csv'),
+                                        os.path.join(Config.dataset_directory, 'new_approach_with_normalization', 'indel_platypus_0.0_all.csv')]
+        }
+
+        feature_selections = {}
+        for k, v in raw_feature_selections.items():
+            feature_selections[data_representation + '_' + k] = v
+
+        model_manager = ModelManager(models, data_representation=data_representation)
+        for k, v in feature_selections.items():
+            print("Models would be trained and tested for feature selection method: " + k)
+            raw_label_matrix = FeatureLabelPreparer.get_labels_from_file(os.path.join(Config.dataset_directory, 'labels.csv'))
+            raw_feature_matrix = FeatureLabelPreparer.get_feature_matrix_from_files(v)
+            model_manager.train_and_test_best_models(k, raw_feature_matrix, raw_label_matrix)
 
     elif args['train_best_models']:
         models = args['<models>']
