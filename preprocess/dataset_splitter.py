1b45233d1e8d8ae6949eae6d179e32a5a681782c
herkut <erkuthamdi@gmail.com>
2019-10-14 12:31:35 +0300

    Define how to save training and testing indices into file

--- preprocess/dataset_splitter.py
+++ preprocess/dataset_splitter.py
@@ -1,16 +1,13 @@
-import os 
+import csv
+import os
 import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit
-import numpy as np
+
+from config import Config
 
 
 def filter_out_nan(x, y):
     index_to_remove = y[y.isna()].index
-
-    for i in y.index:
-        print(str(i) + '--' + str(i in x.index))
-        print(str(i) + '--' + str(i in y.index))
-        print()
 
     xx = x.drop(index_to_remove, inplace=False)
     yy = y.drop(index_to_remove, inplace=False)
@@ -45,9 +42,13 @@ if __name__ == '__main__':
     dataset_directory = '/run/media/herkut/herkut/TB_genomes/ar_detection_dataset/'
     dataset = 'dataset-ii'
     data_representation = 'binary'
-    target_drugs = ['Isoniazid', 'Rifampicin', 'Ethambutol', 'Pyrazinamide']
 
-    for td in target_drugs:
+    configuration_file = '/home/herkut/Desktop/ar_detector/configurations/conf.yml'
+
+    raw = open(configuration_file)
+    Config.initialize_configurations(raw)
+
+    for td in Config.target_drugs:
         if dataset == 'dataset-i':
             raw_feature_selections = {'snp_09_bcf_nu_indel_00_platypus_all': [
                     os.path.join(dataset_directory, 'new_approach_with_normalization', 'snp_bcftools_0.9_notunique.csv'),
@@ -65,15 +66,26 @@ if __name__ == '__main__':
         for k, v in raw_feature_selections.items():
             feature_selections[data_representation + '_' + k] = v
 
-        for td in target_drugs:
-            for k, v in feature_selections.items():
-                raw_label_matrix = get_labels_from_file(os.path.join(dataset_directory, label_file))
+        for k, v in feature_selections.items():
+            raw_label_matrix = get_labels_from_file(os.path.join(dataset_directory, label_file))
 
-                raw_feature_matrix = get_feature_matrix_from_files(v)
-                # raw_feature_matrix.to_csv(index=True)
-                x, y = filter_out_nan(raw_feature_matrix, raw_label_matrix[td])
+            raw_feature_matrix = get_feature_matrix_from_files(v)
+            # raw_feature_matrix.to_csv(index=True)
 
-                sss = StratifiedShuffleSplit(test_size=0.2, random_state=0)
+            x, y = filter_out_nan(raw_feature_matrix, raw_label_matrix[td])
 
-                for train_index, test_index in sss.split(x, y):
-                    print("TRAIN:", train_index, "TEST:", test_index)
+            sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=0)
+
+            for train_index, test_index in sss.split(x, y):
+                target_directory = os.path.join(Config.dataset_index_directory + '_' + Config.target_dataset)
+                with open(os.path.join(target_directory,
+                                       td + '_tr_indices.csv'),
+                          'w') as csv_file:
+                    writer = csv.writer(csv_file, delimiter=',')
+                    for x in train_index: writer.writerow([x])
+
+                with open(os.path.join(target_directory,
+                                       td + '_te_indices.csv'),
+                          'w') as csv_file:
+                    writer = csv.writer(csv_file, delimiter=',')
+                    for x in test_index: writer.writerow([x])
