6e713ad0cd17a983f6a291e02d6a5b49f2c13674
herkut <erkuthamdi@gmail.com>
2019-11-21 19:22:48 +0300

    Initialize postprocessing

---
+++ postprocess/postprocessor.py
@@ -0,0 +1,102 @@
+import os
+
+from config import Config
+from postprocess.rf_feature_sorter import RF
+from preprocess.feature_label_preparer import FeatureLabelPreparer
+from run import get_labels_and_raw_feature_selections
+
+
+class PostProcessor:
+    target_genes = ['ahpC', 'eis', 'embA', 'embB', 'embC', 'embR', 'fabG1', 'gidB', 'gyrA', 'gyrB', 'inhA', 'iniA',
+                    'iniC', 'katG', 'manB', 'ndh', 'pncA', 'rmlD', 'rpoB', 'rpsA', 'rpsL', 'rrs', 'tlyA']
+    target_antibiotic_genes = {'Isoniazid': ['ahpC',
+                                             'fagB1',
+                                             'inhA',
+                                             'katG',
+                                             'ndh'],
+                               'Rifampicin': ['rpoB'],
+                               'Ethambutol': ['embA',
+                                              'embB',
+                                              'embC',
+                                              'embR',
+                                              'iniA',
+                                              'iniC',
+                                              'manB',
+                                              'rmlD'],
+                               'Pyrazinamide': ['pncA',
+                                                'rpsA']}
+    target_genes_start_end_positions = None
+
+    def load_start_and_end_positions_for_all_target_genes(self, additional_base_pair_upstream=100):
+        if PostProcessor.target_genes_start_end_positions is None:
+            PostProcessor.target_genes_start_end_positions = {}
+            for gene in PostProcessor.target_genes:
+                with open(os.path.join(Config.target_genes_directory, gene)) as f:
+                    first_line = f.readline()
+                    elements = first_line.split(' ')
+                    tmp = elements[0].replace('>', '').replace('c', '')
+                    tmp_arr = []
+                    for e in tmp.split(':')[1].split('-'):
+                        tmp_arr.append(int(e))
+                    min_el = min(tmp_arr)
+                    max_el = max(tmp_arr)
+                    tmp_arr[0] = min_el
+                    tmp_arr[1] = max_el
+
+                    if (int(tmp_arr[0]) - additional_base_pair_upstream) < 0:
+                        tmp_arr[0] = str(0)
+                    else:
+                        tmp_arr[0] = str(int(tmp_arr[0]) - additional_base_pair_upstream)
+                    PostProcessor.target_genes_start_end_positions[gene] = {'start': int(tmp_arr[0]),
+                                                                            'end': int(tmp_arr[1])}
+
+    def get_mutation_location_from_mutation_key(self, mutation_key):
+        return int(mutation_key.split('_')[0])
+
+    def find_mutated_gene_from_mutation_key(self, mutation_key):
+        mutation_location = self.get_mutation_location_from_mutation_key(mutation_key)
+
+        mutated_gene = None
+        for gene in PostProcessor.target_genes_start_end_positions:
+            if PostProcessor.target_genes_start_end_positions[gene]['start'] < mutation_location < PostProcessor.target_genes_start_end_positions[gene]['end']:
+                mutated_gene = gene
+                break
+
+        return mutated_gene
+
+    def __init__(self):
+        self.load_start_and_end_positions_for_all_target_genes()
+
+
+if __name__ == '__main__':
+    raw = open('/home/herkut/Desktop/ar_detector/configurations/conf.yml')
+    Config.initialize_configurations(raw)
+
+    label_file, raw_feature_selections = get_labels_and_raw_feature_selections('dataset-ii')
+
+    feature_selections = {}
+    for k, v in raw_feature_selections.items():
+        feature_selections['binary' + '_' + k] = v
+
+    for k, v in feature_selections.items():
+        print("Feature importance would be extacted for: " + k)
+        raw_label_matrix = FeatureLabelPreparer.get_labels_from_file(os.path.join(Config.dataset_directory, label_file))
+        raw_feature_matrix = FeatureLabelPreparer.get_feature_matrix_from_files(v)
+
+    pp = PostProcessor()
+
+    results = {}
+
+    for drug in Config.target_drugs:
+        rf = RF(os.path.join('/home/herkut/Desktop/truba/ar_detector_results_dataset-ii_20191118',
+                             'best_models',
+                             'rf_accuracy_phenotype_binary_snp_09_bcf_nu_indel_00_platypus_all',
+                             'rf_' + drug + '.sav'),
+                raw_feature_matrix.columns)
+        most_importance_features = rf.find_most_important_n_features(100)
+        for mif in most_importance_features:
+            print(pp.find_mutated_gene_from_mutation_key(mif[0]))
+
+    print(pp.find_mutated_gene_from_mutation_key('1917972_A_G'))
+
+    print('Zaa')
---
+++ postprocess/rf_feature_sorter.py
@@ -0,0 +1,53 @@
+import os
+
+import joblib
+from tabulate import tabulate
+
+from config import Config
+from preprocess.feature_label_preparer import FeatureLabelPreparer
+from run import get_labels_and_raw_feature_selections
+
+
+class RF:
+    def __init__(self, model_file, feature_name):
+        """
+
+        :param model_file: the full path for the file containing scikit learn random forest model
+        :param feature_name: pandas series containing name for features
+        """
+        self.model = joblib.load(model_file)
+        self.feature_names = feature_name.values
+
+    def find_most_important_n_features(self, n):
+        headers = ['mutation', 'score']
+        values = sorted(zip(self.feature_names, self.model.feature_importances_), key=lambda x: x[1] * -1)
+        """
+        f = open('/home/herkut/Desktop/rf_feature_Isoniazid.txt', 'w')
+        f.write(tabulate(values[:n], headers=headers[:n], tablefmt='grid'))
+        f.close()
+        """
+        return values[:n]
+
+
+if __name__ == '__main__':
+    raw = open('/home/herkut/Desktop/ar_detector/configurations/conf.yml')
+    Config.initialize_configurations(raw)
+
+    label_file, raw_feature_selections = get_labels_and_raw_feature_selections('dataset-ii')
+
+    feature_selections = {}
+    for k, v in raw_feature_selections.items():
+        feature_selections['binary' + '_' + k] = v
+
+    for k, v in feature_selections.items():
+        print("Feature importance would be extacted for: " + k)
+        raw_label_matrix = FeatureLabelPreparer.get_labels_from_file(os.path.join(Config.dataset_directory, label_file))
+        raw_feature_matrix = FeatureLabelPreparer.get_feature_matrix_from_files(v)
+
+    rf = RF('/home/herkut/Desktop/truba/ar_detector_results_dataset-ii_20191118/best_models/rf_accuracy_phenotype_binary_snp_09_bcf_nu_indel_00_platypus_all/rf_Isoniazid.sav',
+            raw_feature_matrix.columns)
+
+    important_features = rf.find_most_important_n_features(100)
+
+
+
