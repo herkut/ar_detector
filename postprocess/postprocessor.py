9a65bca9f9b7a84403067c8a41533c932a58d4be
herkut <erkuthamdi@gmail.com>
2019-11-25 18:21:52 +0300

    Create mechanism to define mutation names for snps

--- postprocess/postprocessor.py
+++ postprocess/postprocessor.py
@@ -5,6 +5,8 @@ from postprocess.rf_feature_extractor import RandomForestFeatureExtractor
 from preprocess.feature_label_preparer import FeatureLabelPreparer
 from run import get_labels_and_raw_feature_selections
 from Bio.Seq import Seq
+from Bio import SeqIO
+import math
 
 
 class PostProcessor:
@@ -13,8 +15,8 @@ class PostProcessor:
 
     codon_to_aminoacid = {'ATG': 'START',
                           'TAA': 'END', 'TGA': 'END', 'TAG': 'END',
-                          'GCT': 'A', 'GCC': 'A', 'GCC': 'A', 'GCG': 'A',
-                          'TTA': 'L', 'TTG': 'L', 'CTT': 'L', 'CTT': 'L', 'CTA': 'L', 'CTG': 'L',
+                          'GCT': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+                          'TTA': 'L', 'TTG': 'L', 'CTT': 'L', 'CTC': 'L', 'CTA': 'L', 'CTG': 'L',
                           'CGT': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R', 'AGA': 'R', 'AGG': 'R',
                           'AAA': 'K', 'AAG': 'K',
                           'AAT': 'N', 'AAC': 'N',
@@ -51,23 +53,34 @@ class PostProcessor:
                                'Pyrazinamide': ['pncA',
                                                 'rpsA']}
     target_genes_start_end_positions = None
-    gene_codons = None
+    genes = None
+    reference_genome = None
 
     def create_codons_for_all_genes(self):
-        PostProcessor.gene_codons = {}
-        for gene in PostProcessor.target_genes:
-            PostProcessor.gene_codons[gene] = self.create_codons_for_gene(gene)
+        PostProcessor.genes = {}
+        for gene_name in PostProcessor.target_genes:
+            gene = {}
+            codons, aminoacids, gene_complement, sequences, sequences_on_first_helix = self.create_codons_for_gene(gene_name)
+            gene['codons'] = codons
+            gene['aminoacids'] = aminoacids
+            gene['gene_complement'] = gene_complement
+            gene['sequence'] = sequences
+            gene['sequence_on_first_helix'] = sequences_on_first_helix
+            PostProcessor.genes[gene_name] = gene
 
-    def create_codons_for_gene(self, gene, additional_base_pair_upstream=100):
+    def create_codons_for_gene(self, gene, additional_base_pair_upstream=99):
         codons = []
-        gene_3_to_5 = False
+        aminoacids = []
+        sequences_on_first_helix = None
+
+        gene_complement = False
         with open(os.path.join(Config.target_genes_directory, gene)) as f:
             first_line = f.readline()
             elements = first_line.split(' ')
             tmp = elements[0].replace('>', '').split(':')[1]
             tmp_arr = tmp.split('-')
             if tmp_arr[0].startswith('c'):
-                gene_3_to_5 = True
+                gene_complement = True
 
             gene_str = ''
             while True:
@@ -76,43 +89,28 @@ class PostProcessor:
                 if not line:
                     break
 
-            if gene_3_to_5:
-                counter = 0
-                tmp_codon = ''
-                """
-                for nucleotide in reversed(gene_str):
-                    if counter > 2:
-                        counter = 0
-                        codons.append(tmp_codon)
-                        tmp_codon = nucleotide
-                    else:
-                        tmp_codon += nucleotide
-                    counter += 1
-                """
-                za = Seq(gene_str)
-                for nucleotide in za:
-                    if counter > 2:
-                        counter = 0
-                        codons.append(tmp_codon)
-                        tmp_codon = nucleotide
-                    else:
-                        tmp_codon += nucleotide
-                    counter += 1
+            gene_str = Seq(gene_str)
 
+            counter = 0
+            tmp_codon = ''
+            for nucleotide in gene_str:
+                if counter > 2:
+                    counter = 0
+                    codons.append(tmp_codon)
+                    aminoacids.append(PostProcessor.codon_to_aminoacid[tmp_codon])
+                    tmp_codon = nucleotide
+                else:
+                    tmp_codon += nucleotide
+                counter += 1
+
+            if gene_complement:
+                sequences_on_first_helix = gene_str.reverse_complement()
             else:
-                counter = 0
-                tmp_codon = ''
-                for nucleotide in gene_str:
-                    if counter > 2:
-                        counter = 0
-                        codons.append(tmp_codon)
-                        tmp_codon = nucleotide
-                    else:
-                        tmp_codon += nucleotide
-                    counter += 1
-        return codons
+                sequences_on_first_helix = gene_str
 
-    def load_start_and_end_positions_for_all_target_genes(self, additional_base_pair_upstream=100):
+        return codons, aminoacids, gene_complement, gene_str, sequences_on_first_helix
+
+    def load_start_and_end_positions_for_all_target_genes(self, additional_base_pair_upstream=0):
         if PostProcessor.target_genes_start_end_positions is None:
             PostProcessor.target_genes_start_end_positions = {}
             for gene in PostProcessor.target_genes:
@@ -138,37 +136,97 @@ class PostProcessor:
     def get_mutation_location_from_mutation_key(self, mutation_key):
         return int(mutation_key.split('_')[0])
 
-    def find_mutated_gene_from_mutation_key(self, mutation_key):
+    def find_mutated_gene_from_mutation_key(self, mutation_key, additional_base_pair_upstream=100):
         mutation_location = self.get_mutation_location_from_mutation_key(mutation_key)
 
         mutated_gene = None
         for gene in PostProcessor.target_genes_start_end_positions:
-            if PostProcessor.target_genes_start_end_positions[gene]['start'] < mutation_location < PostProcessor.target_genes_start_end_positions[gene]['end']:
+            if PostProcessor.target_genes_start_end_positions[gene]['start']-additional_base_pair_upstream < mutation_location < PostProcessor.target_genes_start_end_positions[gene]['end']:
                 mutated_gene = gene
                 break
 
         return mutated_gene
 
     def __init__(self):
+        self.reference_genome = list(SeqIO.parse('/run/media/herkut/herkut/TB_genomes/reference_genome/mtb_h37rv_v3.fasta', 'fasta'))[0]._seq
         self.load_start_and_end_positions_for_all_target_genes()
         self.create_codons_for_all_genes()
 
+    def find_important_mutations(self, most_importance_features):
+        important_mutations = []
+        for mif in most_importance_features:
+            mutation_name = ''
+            mutated_gene = pp.find_mutated_gene_from_mutation_key(mif[0])
+            location = int(mif[0].split('_')[0])
+            mutation_from = mif[0].split('_')[1]
+            mutation_to = mif[0].split('_')[2]
+            if len(mutation_from) == 1 and len(mutation_to) == 1:
+                mutation_type = 'snp'
+            else:
+                mutation_type = 'indel'
+            if mutation_type == 'snp':
+                if pp.genes[mutated_gene]['gene_complement']:
+                    mutation_to = Seq(mutation_to).complement()
+                    location_on_gene = location - pp.target_genes_start_end_positions[mutated_gene]['start']
+                    location_on_gene = pp.target_genes_start_end_positions[mutated_gene]['end'] - \
+                                       pp.target_genes_start_end_positions[mutated_gene]['start'] - location_on_gene
+                else:
+                    location_on_gene = location - pp.target_genes_start_end_positions[mutated_gene]['start']
+
+                if location_on_gene >= 0:  # mutations on genes
+                    codon_number = int(location_on_gene / 3)
+                    codon_location = location_on_gene % 3
+                    codon_from = pp.genes[mutated_gene]['codons'][codon_number]
+                    codon_to = codon_from
+                    codon_to = codon_to[:codon_location] + mutation_to + codon_to[codon_location + 1:]
+
+                    mutation_name = mutated_gene + '_' + pp.codon_to_aminoacid[codon_from] + str(codon_number + 1) + \
+                                    pp.codon_to_aminoacid[codon_to]
+
+                    print(mutation_name, str(mif[1]))
+                    important_mutations.append({'mutation': mutation_name, 'score': mif[1]})
+                else:  # mutations on gene promoters
+                    """
+                    codon_number = math.floor(location_on_gene / 3)
+                    codon_location = location_on_gene % 3
+                    if codon_location == 0:
+                        codon_st = location
+                        codon_end = location + 2
+                    elif codon_location == 1:
+                        codon_st = location - 1
+                        codon_end = location + 1
+                    elif codon_location == 2:
+                        codon_st = location - 2
+                        codon_end = location
+
+                    codon_from = pp.reference_genome[codon_st:codon_end+1]._data
+                    codon_to = codon_from
+                    codon_to = codon_to[:codon_location] + mutation_to + codon_to[codon_location+1:]
+
+                    if pp.genes[mutated_gene]['gene_complement']:
+                        codon_from = Seq(codon_from).reverse_complement()._seq
+                        codon_to = Seq(codon_to).reverse_complement()._seq
+
+                    mutation_name = mutated_gene + '_' + pp.codon_to_aminoacid[codon_from] + str(codon_number) + pp.codon_to_aminoacid[codon_to]
+                    print(mutation_name, str(mif[1]))
+                    """
+                    if pp.genes[mutated_gene]['gene_complement']:
+                        mutation_from = Seq(mutation_from).reverse_complement()._data
+                        mutation_to = Seq(mutation_to).reverse_complement()._data
+                    mutation_name = mutated_gene + '_' + mutation_from + str(location_on_gene) + mutation_to
+                    print(mutation_name, str(mif[1]))
+                    important_mutations.append({'mutation': mutation_name, 'score': mif[1]})
+            else:
+                print('Mutation is an indel not a snp: ' + mutation_from + ' -> ' + mutation_to)
+
+        return important_mutations
+
 
 if __name__ == '__main__':
-    raw = open('/run/media/herkut/hdd-1/TB_genomes/ar_detector/configurations/conf.yml')
+    raw = open('/home/herkut/Desktop/ar_detector/configurations/conf.yml')
     Config.initialize_configurations(raw)
 
     pp = PostProcessor()
-
-    print(pp.gene_codons['katG'][313] + ' - ' + pp.codon_to_aminoacid[pp.gene_codons['katG'][313]])
-    print(pp.gene_codons['katG'][314] + ' - ' + pp.codon_to_aminoacid[pp.gene_codons['katG'][314]])
-    print(pp.gene_codons['katG'][315] + ' - ' + pp.codon_to_aminoacid[pp.gene_codons['katG'][315]])
-    print(pp.gene_codons['katG'][316] + ' - ' + pp.codon_to_aminoacid[pp.gene_codons['katG'][316]])
-
-    print(pp.gene_codons['fabG1'][13] + ' - ' + pp.codon_to_aminoacid[pp.gene_codons['fabG1'][13]])
-    print(pp.gene_codons['fabG1'][14] + ' - ' + pp.codon_to_aminoacid[pp.gene_codons['fabG1'][14]])
-    print(pp.gene_codons['fabG1'][15] + ' - ' + pp.codon_to_aminoacid[pp.gene_codons['fabG1'][15]])
-    print(pp.gene_codons['fabG1'][16] + ' - ' + pp.codon_to_aminoacid[pp.gene_codons['fabG1'][16]])
 
     label_file, raw_feature_selections = get_labels_and_raw_feature_selections('dataset-ii')
 
@@ -192,11 +250,6 @@ if __name__ == '__main__':
 
         most_importance_features = rf.find_most_important_n_features(50)
         print('Found feature importance for: ' + drug)
-        for mif in most_importance_features:
-            print(pp.find_mutated_gene_from_mutation_key(mif[0]) + ' at ' + str(mif[0].split('_')[0]) +
-                  ' mutation ' + str(mif[0].split('_')[1]) + ' -> ' + str(mif[0].split('_')[2]) +
-                  ' with score ' + str(mif[1]))
-
-    print(pp.find_mutated_gene_from_mutation_key('1917972_A_G'))
+        important_mutations = pp.find_important_mutations(most_importance_features)
 
     print('Zaa')
